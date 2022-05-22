46c0e15c73ae98dfe566821a588a986c10a3e47e
Minsu Han <mshanobject@gmail.com>
2022-05-22 21:19:09 +0900

    feat: Sign out and Redirect to Home using 'useNavigate()'

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../routes/Home';
-import Auth from '../routes/Auth';
+import Navigation from 'components/Navigation';
+import Home from 'routes/Home';
+import Profile from 'routes/Profile';
+import Auth from 'routes/Auth';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />,
+          <Route path="/profile" element={<Profile />} />
         ) : (
           <Route path="/" element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,17 @@
-import React from "react"
+import React from 'react';
+import { authService } from 'fbase';
+import { useNavigate } from 'react-router-dom';
 
-export default () => <span>Profile</span>
+const Profile = () => {
+  const navigate = useNavigate();
+  const onSignOutClick = () => {
+    authService.signOut();
+    navigate('/');
+  };
+  return (
+    <>
+      <button type="button" onClick={onSignOutClick}>Log out</button>
+    </>
+  );
+};
+export default Profile;
